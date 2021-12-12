2b6c026976a827b531acf4cc12f71ad6f25b2a2a
roman18000 <roman18000@gmail.com>
2021-12-12 20:17:09 -0300

    Arreglo de ABM en appointments

--- web/app/controllers/appointments_controller.rb
+++ web/app/controllers/appointments_controller.rb
@@ -23,10 +23,10 @@ class AppointmentsController < ApplicationController
   # POST /appointments or /appointments.json
   def create
     @appointment = Appointment.new(appointment_params)
-
+    @appointment.professional_id = @professional.id
     respond_to do |format|
       if @appointment.save
-        format.html { redirect_to @appointment, notice: "Appointment was successfully created." }
+        format.html { redirect_to [@professional, @appointment], notice: "Appointment was successfully created." }
         format.json { render :show, status: :created, location: @appointment }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class AppointmentsController < ApplicationController
   def update
     respond_to do |format|
       if @appointment.update(appointment_params)
-        format.html { redirect_to @appointment, notice: "Appointment was successfully updated." }
+        format.html { redirect_to [@professional, @appointment], notice: "Appointment was successfully updated." }
         format.json { render :show, status: :ok, location: @appointment }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class AppointmentsController < ApplicationController
   def destroy
     @appointment.destroy
     respond_to do |format|
-      format.html { redirect_to appointments_url, notice: "Appointment was successfully destroyed." }
+      format.html { redirect_to professional_appointments_url, notice: "Appointment was successfully destroyed." }
       format.json { head :no_content }
     end
   end
--- web/app/models/appointment.rb
+++ web/app/models/appointment.rb
@@ -6,4 +6,5 @@ class Appointment < ApplicationRecord
   #ejemplo para poner mensaje "validates :name, presence: { message: 'debe colocar un nombre'}"
   #despues en una consola por ejemplo cuando intento instanciar un objeto me da error y si miro
   #los errores con instancia.errors.full_messages salta el error
+
 end
